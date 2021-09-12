12f462f3b8050aa44b6792b6bcd8ad4724e7d235
Sonia brahman <86123814+Sonia0612@users.noreply.github.com>
2021-09-12 16:01:23 +0530

    Add files via upload

---
+++ isoceles pattern.py
@@ -0,0 +1,23 @@
+
+#using while function
+def while_func(n):
+    i=1
+    while i<=n:
+        #spaces
+        j=1
+        while j<=n-i:
+            print(" ",end="")
+            j=j+1
+        #stars
+        k=i
+        while k<=2*i-1:
+            print(k,end="")
+            k=k+1
+        k=k-2
+        while k>=i:
+            print(k,end="")
+            k=k-1
+        i=i+1
+        print()
+n=int(input())
+while_func(n)
